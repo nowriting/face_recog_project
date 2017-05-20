84d2c5bf87fed79f821be97fac3b04a6a0138278
eliza <no.writing@gmail.com>
2017-05-20 19:16:52 +0300

    loop and detect a test directory

--- Salamander/Salamander/Program.cs
+++ Salamander/Salamander/Program.cs
@@ -13,6 +13,7 @@ using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.UI;
 using Emgu.CV.Cuda;
+using System.IO;
 
 namespace Salamander
 {
@@ -39,47 +40,59 @@ namespace Salamander
         static void Run()
         {
             IImage image;
-
+            // getting image from project file directory
+            // string imgFromFile = @"testImages\ilva.jpg";
+            string folder = @"testImages";
             //Read the files as an 8-bit Bgr image  
 
-            image = new UMat("ilva.jpg", ImreadModes.Color); //UMat version
-                                                             //image = new Mat("lena.jpg", ImreadModes.Color); //CPU version
+            var files = Directory.GetFiles(folder, "*.jpg", SearchOption.AllDirectories);
+            List<string> imageFiles = new List<string>();
+            foreach (string filename in files)
+            {
+                image = new UMat(filename, ImreadModes.Color);
 
-            long detectionTime;
-            List<Rectangle> faces = new List<Rectangle>();
-            List<Rectangle> eyes = new List<Rectangle>();
 
-            DetectFace.Detect(
-              image, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
-              faces, eyes,
-              out detectionTime);
+                //     image = new UMat(imgFromFile, ImreadModes.Color); //UMat version
+                //image = new Mat("lena.jpg", ImreadModes.Color); //CPU version
 
-            foreach (Rectangle face in faces) {
-                // the recangle drawn in the found face
-                CvInvoke.Rectangle(image, face, new Bgr(Color.Black).MCvScalar, 3);
-                // source image converted to bitmap
-                Bitmap bmpImg = image.Bitmap;
-                // a call to cropImage function, input: source and rectangle
-                Bitmap CroppedImage = CropImage(bmpImg, face);
-                // cropped bitmap converted to image
-                Image<Bgr, Byte> myImage = new Image<Bgr, Byte>(CroppedImage);
-                // image displayed
-                CvInvoke.Imshow("picture", myImage);
+                long detectionTime;
+                List<Rectangle> faces = new List<Rectangle>();
+                List<Rectangle> eyes = new List<Rectangle>();
+
+                DetectFace.Detect(
+                  image, "haarcascade_frontalface_default.xml", "haarcascade_eye.xml",
+                  faces, eyes,
+                  out detectionTime);
+
+
+                foreach (Rectangle face in faces)
+                {
+                    // the recangle drawn in the found face
+                    CvInvoke.Rectangle(image, face, new Bgr(Color.Black).MCvScalar, 3);
+                    // source image converted to bitmap
+                    Bitmap bmpImg = image.Bitmap;
+                    // a call to cropImage function, input: source and rectangle
+                    Bitmap CroppedImage = CropImage(bmpImg, face);
+                    // cropped bitmap converted to image
+                    Image<Bgr, Byte> myImage = new Image<Bgr, Byte>(CroppedImage);
+                    // image displayed
+                    CvInvoke.Imshow("picture", myImage);
+                }
+
+                foreach (Rectangle eye in eyes)
+                    CvInvoke.Rectangle(image, eye, new Bgr(Color.AntiqueWhite).MCvScalar, 3);
+
+                //display the image 
+                using (InputArray iaImage = image.GetInputArray())
+                    ImageViewer.Show(image, String.Format(
+                       "Sejas, acu un mutes atpazīšana {0} milisekundēs: {1}s",
+                       (iaImage.Kind == InputArray.Type.CudaGpuMat && CudaInvoke.HasCuda) ? "CUDA" :
+                       (iaImage.IsUMat && CvInvoke.UseOpenCL) ? "OpenCL"
+                       : "CPU",
+                       detectionTime));
             }
-                
-            foreach (Rectangle eye in eyes)
-                CvInvoke.Rectangle(image, eye, new Bgr(Color.AntiqueWhite).MCvScalar, 3);
-
-            //display the image 
-            using (InputArray iaImage = image.GetInputArray())
-                ImageViewer.Show(image, String.Format(
-                   "Sejas, acu un mutes atpazīšana {0} milisekundēs: {1}s",
-                   (iaImage.Kind == InputArray.Type.CudaGpuMat && CudaInvoke.HasCuda) ? "CUDA" :
-                   (iaImage.IsUMat && CvInvoke.UseOpenCL) ? "OpenCL"
-                   : "CPU",
-                   detectionTime));
         }
-
+        
 
         public static Bitmap CropImage(Bitmap source, Rectangle section)
         {
